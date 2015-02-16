d5822488cf2966567d80af5a94eff03a83553fc4
nate stephens <ndstephens@gmail.com>
2015-02-16 16:29:58 -0700

    started project

---
+++ DailyProgrammer-Template/DailyProgrammer-Template/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; //test classes need to have the using statement
+
+///     REDDIT DAILY PROGRAMMER SOLUTION TEMPLATE 
+///                             http://www.reddit.com/r/dailyprogrammer
+///     Your Name: Nate Stephens    
+///     Challenge Name:  Rail Fence Ciper
+///     Challenge #: 196
+///     Challenge URL: http://www.reddit.com/r/dailyprogrammer/comments/2rnwzf/20150107_challenge_196_intermediate_rail_fence/
+///     Brief Description of Challenge:  Encrypt and Decrypt a string using the rail fence method
+///
+/// 
+///
+///     What was difficult about this challenge?
+/// 
+///
+///     
+///
+///     What was easier than expected about this challenge?
+///     
+///
+///
+///
+///     BE SURE TO CREATE AT LEAST TWO TESTS FOR YOUR CODE IN THE TEST CLASS
+///     One test for a valid entry, one test for an invalid entry.
+
+namespace DailyProgrammer_Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Simple function to illustrate how to use tests
+        /// </summary>
+        /// <param name="inputInteger"></param>
+        /// <returns></returns>
+        public static int MyTestFunction(int inputInteger)
+        {
+            return inputInteger;
+        }
+    }
+
+    class RailFenceCipher
+    {
+
+        public static string Encrypt(string encryptText, int numLines)
+        {
+
+            return string.Empty;
+        }
+    }
+
+
+#region " TEST CLASS "
+
+    //We need to use a Data Annotation [ ] to declare that this class is a Test class
+    [TestFixture]
+    class Test
+    {
+        //Test classes are declared with a return type of void.  Test classes also need a data annotation to mark them as a Test function
+        [Test]
+        public void MyValidTest()
+        {
+            //inside of the test, we can declare any variables that we'll need to test.  Typically, we will reference a function in your main program to test.
+            int result = Program.MyTestFunction(15);  // this function should return 15 if it is working correctly
+            //now we test for the result.
+            Assert.IsTrue(result == 15, "This is the message that displays if it does not pass");
+            // The format is:
+            // Assert.IsTrue(some boolean condition, "failure message");
+        }
+
+        [Test]
+        public void MyInvalidTest()
+        {
+            int result = Program.MyTestFunction(15);
+            Assert.IsFalse(result == 14);
+        }
+    }
+#endregion
+}
